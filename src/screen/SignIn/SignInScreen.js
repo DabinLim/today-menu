0c2723dab862c21fd2fe0561df1c4ec3facdba9f
Dabin <dabinim09@gmail.com>
2021-08-22 18:23:54 +0900

    fix:initialize app

---
+++ src/screen/SignIn/SignInScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const SignInScreen = () => (
+  <View>
+    <Text style={styles.container}>
+      Sign In Page
+    </Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
+
+export default SignInScreen;
